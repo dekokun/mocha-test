3cc184b7d79e7538da654b15d3362a624a54543d
dekokun <shintaro.kurachi@gmail.com>
2012-03-12 20:30:34 +0900

    first commit

---
+++ test/calc.test.js
@@ -0,0 +1,17 @@
+var calc = require('../lib/calc');
+
+var should = require('should');
+
+describe('calc', function() {
+  describe('.add', function() {
+    it('should return sum of 2 arguments', function() {
+      var result = calc.add(1, 2);
+      result.should.equal(3);
+    });
+
+    it('error', function() {
+      var result = calc.add(1, 3);
+      result.should.equal(3);
+    });
+  });
+});
